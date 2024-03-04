ecbe84f4f47fb7e9cf87fce7339ff861492ce51d
weihao <weihao@xiaomi.com>
2024-03-04 14:12:01 +0800

    搭建niolan框架

---
+++ android/app/src/main/java/com/niolan/text/AppActivity.kt
@@ -0,0 +1,14 @@
+package com.niolan.text
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.niolan.R
+import com.niolan.databinding.ActivityAppBinding
+import com.xxxd86.niobase.base.baseActivity.BaseActivityLate
+
+class AppActivity : BaseActivityLate<ActivityAppBinding>(ActivityAppBinding::inflate) {
+    override fun initView() {
+
+
+    }
+}
